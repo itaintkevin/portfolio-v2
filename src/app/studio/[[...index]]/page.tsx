23f0857ebb854709a8a71851860d4b439b50eccd
itaintkevin <kpjtofficial@gmail.com>
2023-06-08 20:51:24 +0530

    fix: Error fixes

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -2,11 +2,6 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 
-export const metadata = {
-  title: "Kevin Paul | Studio",
-  description: 'Sanity Studio',
-}
-
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
